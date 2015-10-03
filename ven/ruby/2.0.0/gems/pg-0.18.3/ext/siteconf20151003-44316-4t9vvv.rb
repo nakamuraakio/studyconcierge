3da3db414fd35637b4d4e81dd56165f526a006e8
kazukiyo923 <kazukiyo923@gmail.com>
2015-10-03 18:14:46 +0900

    チューター管理画面のフロント実装

---
+++ ven/ruby/2.0.0/gems/pg-0.18.3/ext/siteconf20151003-44316-4t9vvv.rb
@@ -0,0 +1,6 @@
+require 'rbconfig'
+dest_path = "./.gem.20151003-44316-1loxi12"
+RbConfig::MAKEFILE_CONFIG['sitearchdir'] = dest_path
+RbConfig::CONFIG['sitearchdir'] = dest_path
+RbConfig::MAKEFILE_CONFIG['sitelibdir'] = dest_path
+RbConfig::CONFIG['sitelibdir'] = dest_path
