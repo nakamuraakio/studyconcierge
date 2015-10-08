005e26a137550ca51c8ee9d5f53a094d2569f626
中村 彰男 <nakamuraakio@nakamura-no-MacBook-Pro.local>
2015-10-08 14:43:30 +0900

    tutorが指導中でない学生のsummaryを見れてしまうバグの修正

--- app/controllers/tutor_see_summary_controller.rb
+++ app/controllers/tutor_see_summary_controller.rb
@@ -8,9 +8,22 @@ class TutorSeeSummaryController < ApplicationController
 
   def show
     @summary = Summary.find(params[:id])
+    #現在指導中でない学生の報告は読めないようにする
+    count = 0
+    current_tutor.users.each do |user|
+      if @summary.user == user
+        break
+      else
+        count += 1
+      end
+    end
+    if count == current_tutor.users.count
+      render '/public/404.html'
+    end
+
     @reports = @summary.reports.includes(:user)
     if @summary.comments.any?
-      @comments = @summary.comments
+      @comments = @summary.comments.includes(:tutor).includes(:user)
       @comments.each do |comment|
         if !comment.read_flag && comment.created_by_user
           comment.update(:read_flag => true)
