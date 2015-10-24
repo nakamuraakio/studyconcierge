8cd300084f5ab961c0e46e397b51c95fbfd97d77
中村 彰男 <nakamuraakio@nakamura-no-MacBook-Pro.local>
2015-10-25 00:26:36 +0900

    LPの大幅拡充

--- app/models/inquiry.rb
+++ app/models/inquiry.rb
@@ -4,6 +4,7 @@ class Inquiry
   attr_accessor :name, :email, :message
  
   validates :name, :presence => {:message => '名前を入力してください'}
-  validates :email, :presence => {:message => 'メールアドレスを入力してください'}
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :email, :presence => {:message => 'メールアドレスを入力してください'}, format: { with: VALID_EMAIL_REGEX }
   validates :message, :presence => true
 end
